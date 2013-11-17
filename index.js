ded2cfc179ca2ca46d5f0f6e951723be1128dc57
Nathan Rajlich <nathan@tootallnate.net>
2013-11-16 17:34:46 -0800

    index: add support for calling the proxy function directly
    For those times when you don't want to extend the prototype...
    Still needs tests...

--- index.js
+++ index.js
@@ -15,22 +15,55 @@ var SocksProxyAgent = require('socks-proxy-agent');
 module.exports = setup;
 
 /**
+ * The built-in proxy types.
+ */
+
+var defaultProxies = {
+  'http': httpOrHttpsProxy,
+  'https': httpOrHttpsProxy,
+  'socks': socksProxy
+};
+
+/**
  * Adds a `.proxy(uri)` function to the "superagent" module's Request class.
  * No `proxyAgents` are added by default. You must add an `http.Agent` subclass
  * (like HTTP proxy, HTTPS proxy, and SOCKS) to handle the connection internally.
+ *
+ * ``` js
+ * var request = require('superagent');
+ * require('superagent-proxy')(request);
+ *
+ * request
+ *   .get(uri)
+ *   .proxy(uri)
+ *   .end(fn);
+ * ```
+ *
+ * Or, you can pass in a `superagent.Request` instance, and it's like calling the
+ * proxy function on it without extending the prototype:
+ *
+ * ``` js
+ * var request = require('superagent');
+ * var proxy = require('superagent-proxy');
+ *
+ * proxy(request.get(uri), uri).end(fn);
+ * ```
  *
  * @param {Object} superagent The `superagent` exports object
  * @api public
  */
 
-function setup (superagent) {
+function setup (superagent, uri) {
   var Request = superagent.Request;
-  superagent.proxies = Request._proxies = {
-    'http': httpOrHttpsProxy,
-    'https': httpOrHttpsProxy,
-    'socks': socksProxy
-  };
-  Request.prototype.proxy = proxy;
+  if (Request) {
+    // the superagent exports object - extent Request with "proxy"
+    superagent.proxies = Request._proxies = Object.create(defaultProxies);
+    Request.prototype.proxy = proxy;
+    return superagent;
+  } else {
+    // assume it's a `superagent.Request` instance
+    return proxy.call(superagent, uri);
+  }
 }
 
 /**
@@ -41,7 +74,7 @@ function setup (superagent) {
  */
 
 function proxy (uri) {
-  var proxies = this.constructor._proxies;
+  var proxies = this.constructor._proxies || defaultProxies;
 
   // parse the URI into an opts object if it's a String
   var proxyParsed = uri;
