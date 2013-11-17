3bb1f490d6d965c52e5548df1e89f8d9ba1d1dfd
Nathan Rajlich <nathan@tootallnate.net>
2013-11-16 17:15:23 -0800

    cleanup and refactor to allow for an opts object to be used
    Also paving the way for caching the http.Agent on a per-proxy basis.
    Planning on using lru-cache for that.

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ module.exports = setup;
 function setup (superagent) {
   var Request = superagent.Request;
   superagent.proxies = Request._proxies = {
-    'http:': httpOrHttpsProxy,
-    'https:': httpOrHttpsProxy,
-    'socks:': socksProxy
+    'http': httpOrHttpsProxy,
+    'https': httpOrHttpsProxy,
+    'socks': socksProxy
   };
   Request.prototype.proxy = proxy;
 }
@@ -42,17 +42,47 @@ function setup (superagent) {
 
 function proxy (uri) {
   var proxies = this.constructor._proxies;
+
+  // parse the URI into an opts object if it's a String
   var proxyParsed = uri;
   if ('string' == typeof uri) {
     proxyParsed = url.parse(uri);
   }
-  var proxyFn = proxies[proxyParsed.protocol];
-  if (!proxyFn) throw new Error('unsupported proxy protocol: "' + proxyParsed.protocol + '"');
 
-  var agent = proxyFn(this, proxyParsed);
+  // get the requested "protocol"
+  var protocol = proxyParsed.protocol;
+  if (!protocol) {
+    var types = Object.keys(proxies);
+    throw new TypeError('you must specify a string "protocol" for the proxy type (' + types.join(', ') + ')');
+  }
+
+  // strip the trailing ":" if present
+  if (':' == protocol[protocol.length - 1]) {
+    protocol = protocol.substring(0, protocol.length - 1);
+  }
+
+  // get the proxy Agent creation function
+  var proxyFn = proxies[protocol];
+  if ('function' != typeof proxyFn) {
+    throw new TypeError('unsupported proxy protocol: "' + protocol + '"');
+  }
+
+  // format the proxy info back into a URI, since an opts object
+  // could have been passed in originally. This generated URI will
+  // be used for caching soon
+  var proxyUri = url.format({
+    protocol: protocol,
+    slashes: true,
+    hostname: proxyParsed.hostname || proxyParsed.host,
+    port: proxyParsed.port
+  });
+
+  // get an `http.Agent` instance from protocol-specific agent function
+  // TODO: implement caching based on the proxyUri
+  var agent = proxyFn(this, proxyParsed, proxyUri);
   if (agent) {
     this._proxy = proxyParsed;
-    this._proxyUri = uri;
+    this._proxyUri = proxyUri;
     this.agent(agent);
   }
 
@@ -61,7 +91,7 @@ function proxy (uri) {
 
 
 /**
- * Default "http:" and "https:" proxy uri handlers.
+ * Default "http" and "https" proxy URI handlers.
  *
  * @api protected
  */
@@ -78,7 +108,7 @@ function httpOrHttpsProxy (req, proxy) {
 }
 
 /**
- * Default "socks:" proxy uri handler.
+ * Default "socks" proxy URI handler.
  *
  * @api protected
  */
